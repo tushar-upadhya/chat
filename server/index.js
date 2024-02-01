740a7e35c2bff7ab1d3b2a7d80b6ba5f1d379d25
tushar-upadhya <tusharupadhyay691@gmail.com>
2024-02-02 01:07:14 +0530

    making server routes

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
-console.log("a");
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => console.log(`listening ${PORT}`));
